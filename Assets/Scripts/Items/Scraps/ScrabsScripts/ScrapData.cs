a47d785eb4b5acba1c97f8aba0cb06b5baf7e20f
kitsune4864 <necsinete9050@gmail.com>
2024-08-02 19:22:11 +0900

    파일 정리 & 수정사항

---
+++ Assets/Scripts/Items/Scraps/ScrabsScripts/ScrapData.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScrapData", menuName = "Scriptable Objects/ScrapData", order = 0)]
+public class ScrapData : ScriptableObject
+{
+    [SerializeField] 
+    private string scrapName; 
+    public string ScrapName
+    {
+        get { return scrapName; }
+    }
+
+    [SerializeField] 
+    private int scrapPrice;
+    public int ScrapPrice
+    {
+        get { return scrapPrice; }
+    }
+
+    [SerializeField]
+    private int scrapWeight;
+    public int ScrapWeight
+    {
+        get { return scrapWeight; }
+    }
+
+    [SerializeField] 
+    private GameObject scrapPrefab;
+
+    public GameObject ScrapPrefab
+    {
+        get {
+            return scrapPrefab;
+        }
+    }
+    [SerializeField] 
+    private Sprite scrapIcon;
+
+    public Sprite ScrapIcon
+    {
+        get {
+            return scrapIcon;
+        }
+    }
+}
