ddc551139f5f6652e417a6feee0daa8150f5e858
kitsune4864 <necsinete9050@gmail.com>
2024-08-05 10:17:06 +0900

    맵 추가 생성 사항 및 asset 파일 + 캐비넷 오픈

---
+++ Assets/Scripts/Map/RedStorageLDoor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class RedStorageLDoor : MonoBehaviour
+{
+    public float openAngle;
+
+    public float openSpeed = 0.5f;
+
+    private bool isOpen = false;
+
+    private Quaternion defaultRotation;
+
+    private Quaternion openRotation;
+    void Start()
+    {
+        defaultRotation = transform.rotation;
+        openRotation = Quaternion.Euler(defaultRotation.eulerAngles + Vector3.up * openAngle);
+    }
+    
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && !isOpen)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                isOpen = true;
+                StartCoroutine(OpenDoor());
+            }
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * openSpeed;
+            transform.rotation = Quaternion.Lerp(defaultRotation, openRotation, t);
+            yield return null;
+        }
+    }
+}
