2be0b7e1f1013a7ab7eb8ee5b339511ab8869e8a
kitsune4864 <necsinete9050@gmail.com>
2024-08-05 12:00:58 +0900

    맵 파일 및 asset 추가

--- Assets/Scripts/Map/RedStorageRDoor.cs
+++ Assets/Scripts/Map/RedStorageRDoor.cs
@@ -1,34 +1,45 @@
+using System;
 using System.Collections;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class RedStorageRDoor : MonoBehaviour
 {
     public float openAngle;
-
+    public float closeAngle;
     public float openSpeed = 0.5f;
+    public float closeSpeed = 0.5f;
 
     private bool isOpen = false;
 
     private Quaternion defaultRotation;
-
     private Quaternion openRotation;
+    private Quaternion closeRotation;
     void Start()
     {
         defaultRotation = transform.rotation;
         openRotation = Quaternion.Euler(defaultRotation.eulerAngles + Vector3.up * openAngle);
+        closeRotation = Quaternion.Euler(defaultRotation.eulerAngles + Vector3.up * closeAngle);
     }
     
     void OnTriggerStay(Collider other)
     {
-        
-            if (other.CompareTag("Player") && !isOpen)
+        if (other.CompareTag("Player") && !isOpen)
+        {
+            if(Input.GetKey(KeyCode.E))
             {
-                if(Input.GetKey(KeyCode.E))
-                {
-                   isOpen = true;
-                   StartCoroutine(OpenDoor());
-                }
+                isOpen = true; 
+                StartCoroutine(OpenDoor());
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(CloseDoor());
+        }
     }
 
     IEnumerator OpenDoor()
@@ -38,6 +49,17 @@ public class RedStorageRDoor : MonoBehaviour
         {
             t += Time.deltaTime * openSpeed;
             transform.rotation = Quaternion.Slerp(defaultRotation, openRotation, t);
+            yield return null;
+        }
+    }
+
+    IEnumerator CloseDoor()
+    {
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * closeSpeed;
+            transform.rotation = Quaternion.Slerp(defaultRotation, closeRotation, t);
             yield return null;
         }
     }
