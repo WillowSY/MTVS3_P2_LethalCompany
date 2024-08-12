e7f470638a71a364cee265f260a8bcd06677f8ab
이서영 <88702441+WillowSY@users.noreply.github.com>
2024-08-12 11:19:24 +0900

    fix : Timer UI 수정

--- Assets/Scripts/Map/ShipController.cs
+++ Assets/Scripts/Map/ShipController.cs
@@ -19,7 +19,7 @@ public class ShipController : MonoBehaviour
     {
         Vector3 destination = new Vector3(19f, 0.5f, -33f);
         Vector3 speed = Vector3.zero;
-        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, destination, ref speed, 0.3f);
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, destination, ref speed, 1f);
         
     }
 
